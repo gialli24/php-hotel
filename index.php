ee483195bd4af2385c6d8957bdfcc3253d07066a
gialli24 <a.gialli2409@gmail.com>
2026-06-17 15:40:08 +0200

    add bootstrap and render data in a table

--- index.php
+++ index.php
@@ -38,11 +38,64 @@ $hotels = [
     ],
 
 ];
+?>
 
-foreach ($hotels as $hotel) {
-    foreach ($hotel as $key => $value) {
-        echo "$key - $value ";
-    }
+<!DOCTYPE html>
+<html lang="en">
 
-    echo "<br>";
-}
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="app-main">
+
+        <div class="container">
+
+            <h1 class="my-4">Hotels</h1>
+
+            <table
+                class="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Descrizione</th>
+                        <th scope="col">Parcheggio</th>
+                        <th scope="col">Voto</th>
+                        <th scope="col">Distanza dal Centro</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    foreach ($hotels as $hotel) {
+                        echo '<tr class="">';
+
+                        foreach ($hotel as $key => $value) {
+                            if ($key == 'parking') {
+                                $hasParking = $value == true ? 'Si' : 'No';
+                                echo "<td>$hasParking</td>";
+                            } elseif ($key == "distance_to_center") {
+                                echo "<td>$value km</td>";
+                            } else {
+                                echo "<td>$value</td>";
+                            }
+                        }
+
+                        echo '</tr>';
+                    }
+                    ?>
+                </tbody>
+            </table>
+
+        </div>
+
+    </div>
+    <!-- /.app-main -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+
+</html>
